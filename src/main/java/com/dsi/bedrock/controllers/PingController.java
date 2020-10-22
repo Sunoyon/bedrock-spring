35a7c7bdf40268f9c99fbdd1ec8689f44045dce3
sunoyon <sunoyon@gmail.com>
2020-10-22 12:54:04 +0600

    Prometheus configured with micrometer

--- src/main/java/com/dsi/bedrock/controllers/PingController.java
+++ src/main/java/com/dsi/bedrock/controllers/PingController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dsi.bedrock.dto.response.PingResponseDto;
-import com.dsi.bedrock.exception.exceptions.EntityNotFoundException;
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
@@ -14,8 +13,7 @@ public class PingController {
 	@GetMapping(value = "/ping", produces = "application/json")
 	@ApiResponse(responseCode = "200", description = "Service is running")
 	public PingResponseDto getPing() {
-//		return new PingResponseDto("Pong!");
-		throw new EntityNotFoundException("Pong object not found");
+		return new PingResponseDto("Pong!");
 	}
 
 }
